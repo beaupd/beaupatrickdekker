30fc49c966675b21bef19bb567dee67af8511880
beaupd <beau@patrickdekker.nl>
2021-11-08 19:32:21 +0100

    init and start of layout

---
+++ components/layouts/main.js
@@ -0,0 +1,17 @@
+import Head from "next/head"
+
+const Main = () => {
+    return (
+        <div>
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <title>Beau Patrick Dekker</title>
+            </Head>
+            <main>
+                
+            </main>
+        </div>
+    )
+}
+
+export default Main
---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+const options = {
+    arrowParens: "avoid",
+    singleQuote: true,
+    bracketSpacing: true,
+    endOfLine: "lf",
+    semi: false,
+    tabWidth: 4,
+    tralingComma: "none"
+}
+
+module.exports = options
